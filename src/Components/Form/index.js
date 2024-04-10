1d306e1083041b618f6489c9ddaadaa3788c2d91
Kanan048 <159876773+Kanan048@users.noreply.github.com>
2024-04-10 14:42:00 +0530

    data submission & display

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -4,10 +4,100 @@ import Table from "react-bootstrap/Table";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
+import { useState } from "react";
 function FormData() {
+  const [data, setData] = useState([]);
+  const [newRow, setNewRow] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+  });
+
+  const addRow = () => {
+    if (
+      newRow.firstName.trim() !== "" &&
+      newRow.lastName.trim() !== "" &&
+      newRow.email.trim() !== "" &&
+      newRow.phone.trim() !== ""
+    ) {
+      const newData = [...data, { ...newRow, id: data.length + 1 }];
+      setData(newData);
+      setNewRow({
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+      });
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setNewRow((prevRow) => ({
+      ...prevRow,
+      [name]: value,
+    }));
+  };
+
   return (
     <Container fluid>
       <div>
+        <div className="content-form">
+          <div className="form_group">
+            <label>First Name*</label>
+            <br />
+            <input
+              type="text"
+              placeholder="your first name"
+              value={newRow.firstName}
+              name="firstName"
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <div className="form_group">
+            <label>Last Name:</label>
+            <br />
+            <input
+              type="text"
+              placeholder="your last name"
+              value={newRow.lastName}
+              name="lastName"
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <div className="form_group">
+            <label>Email:</label>
+            <br />
+            <input
+              type="text"
+              placeholder="your email"
+              value={newRow.email}
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <div className="form_group">
+            <label>Phone:</label>
+            <br />
+            <input
+              type="text"
+              placeholder="your phone number"
+              value={newRow.phone}
+              name="phone"
+              onChange={handleChange}
+            />
+          </div>
+          <br />
+          <button className="form_group-btn" onClick={addRow}>
+            Submit
+          </button>
+          <br />
+        </div>
+
         <Table striped bordered hover className="list-table">
           <thead>
             <tr>
@@ -20,42 +110,20 @@ function FormData() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-              <td>IND +12487</td>
-              <td>
-                <button className="btn btn-danger">Delete</button>
-                &nbsp;&nbsp;
-                <button className="btn btn-secondary">Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-              <td>AUS +12487</td>
-              <td>
-                <button className="btn btn-danger">Delete</button>
-                &nbsp;&nbsp;
-                <button className="btn btn-secondary">Edit</button>
-              </td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@twitter</td>
-              <td>UK +12487</td>
-              <td>
-                <button className="btn btn-danger">Delete</button>
-                &nbsp;&nbsp;
-                <button className="btn btn-secondary">Edit</button>
-              </td>
-            </tr>
+            {data.map((row, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{row.firstName}</td>
+                <td>{row.lastName}</td>
+                <td>{row.email}</td>
+                <td>{row.phone}</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                  &nbsp;&nbsp;
+                  <button className="btn btn-secondary">Edit</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </Table>
         <div className=" second-form">
